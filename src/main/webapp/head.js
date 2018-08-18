76f3c569cccff946072231ce1a758a0dafa95e58
Feiyu Yue <usts428big2@163.com>
2018-08-18 16:00:57 +0800

    html重复的代码改成js

---
+++ src/main/webapp/head.js
@@ -0,0 +1,203 @@
+document.writeln("<div class='header'>")
+
+document.writeln("<div class='logo'>")
+
+document.writeln(" <a href='index.html'><h1>Fivus</h1></a> </div>")
+
+document.writeln("<div class='search'>")
+
+document.writeln("<div class='search2'>")
+
+document.writeln("<form>")
+
+document.writeln("<i class='fa fa-search'></i>")
+
+document.writeln("<input type='text' value='在此处输入影片片名或主演名字' onfocus='this.value = '';'onblur='if (this.value == '') {this.value = '在此处输入影片片名或主演名字';}'/></form> ")
+
+document.writeln("</div> </div>")
+
+document.writeln("<div class='clearfix'></div></div>")
+
+document.writeln("<div class='bootstrap_container'>")
+
+document.writeln("<nav class='navbar navbar-default w3_megamenu' role='navigation'>")
+
+document.writeln("<div class='navbar-header'>")
+
+document.writeln("<button type='button' data-toggle='collapse' data-target='#defaultmenu' class='navbar-toggle'><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button>")
+
+document.writeln("<a href='index.html' class='navbar-brand'><i class='fa fa-home'></i></a>")
+
+document.writeln("</div><!-- end navbar-header -->")
+
+
+document.writeln("<div id='defaultmenu' class='navbar-collapse collapse'>")
+
+document.writeln("<ul class='nav navbar-nav'>")
+
+document.writeln("<li class='active'><a href='index.html'>首页</a></li>")
+
+document.writeln("<!-- Mega Menu -->")
+
+document.writeln("<li class='dropdown w3_megamenu-fw'><a href='movies.html' data-toggle='dropdown'class='dropdown-toggle'>影片<b class='caret'></b></a>")
+
+document.writeln("<ul class='dropdown-menu fullwidth'>")
+
+document.writeln("<li class='w3_megamenu-content'>")
+
+document.writeln("<div class='row'>")
+
+document.writeln("<h5 class='movies-page'>全部影片 - <a href='movies.html'>点这儿</a>")
+
+document.writeln("</h5>")
+
+document.writeln("<div class='col-sm-4'>")
+
+document.writeln("<h3 class='title'>正在热映</h3>")
+
+document.writeln("<ul class='mov_list'>")
+
+document.writeln("<li>99%</li>")
+
+document.writeln("<li><a href='movie-single.html'>数据库抓取电影名称(Telugu) (U/A)</a></li>")
+
+document.writeln("</ul>")
+
+document.writeln("<ul class='mov_list'>")
+
+document.writeln("<li>100%</li>")
+
+document.writeln("<li><a href='movie-single.html'>数据库抓取电影名称(Hindi) (U/A)</a></li>")
+
+document.writeln("</ul>")
+
+document.writeln("<ul class='mov_list'>")
+
+document.writeln("<li>98%</li>")
+
+document.writeln("<li><a href='movie-single.html'>数据库抓取电影名称(English) (U/A)</a></li>")
+
+document.writeln("</ul>")
+
+document.writeln("<ul class='mov_list'>")
+
+document.writeln("<li>80%</li>")
+
+document.writeln("<li><a href='movie-single.html'>数据库抓取电影名称 (3D Hindi) (U/A)</a></li>")
+
+document.writeln("</ul>")
+
+document.writeln("<ul class='mov_list'>")
+
+document.writeln("<li>65%</li>")
+
+document.writeln("<li><a href='movie-single.html'>数据库抓取电影名称 (U)</a></li>")
+
+document.writeln("</ul> </div><!-- end col-4 -->")
+
+document.writeln("<div class='clearfix'></div>")
+
+document.writeln("<div class='menu-featured-movies'>")
+
+document.writeln("<h3 class='title'>佳片精选</h3>")
+
+document.writeln("<div class='col-md-2 menu-featured-movies-img'>")
+
+document.writeln("<a href='movie-single.html'><img src='http://120.79.35.160/images/mf1.jpg' alt=''/></a>")
+
+document.writeln("</div>")
+
+document.writeln("<div class='col-md-2 menu-featured-movies-img'>")
+
+document.writeln("<a href='movie-single.html'><img src='http://120.79.35.160/images/mf2.jpg' alt=''/></a>")
+
+document.writeln("</div>")
+
+document.writeln("<div class='col-md-2 menu-featured-movies-img'>")
+
+document.writeln("<a href='movie-single.html'><img src='http://120.79.35.160/images/mf3.jpg' alt=''/></a>")
+
+document.writeln("</div>")
+
+document.writeln("<div class='col-md-2 menu-featured-movies-img'>")
+
+document.writeln("<a href='movie-single.html'><img src='http://120.79.35.160/images/mf3.jpg' alt=''/></a>")
+
+document.writeln("</div>")
+
+document.writeln("<div class='col-md-2 menu-featured-movies-img'>")
+
+document.writeln("<a href='movie-single.html'><img src='http://120.79.35.160/images/mf3.jpg' alt=''/></a>")
+
+document.writeln("</div>")
+
+document.writeln("<div class='col-md-2 menu-featured-movies-img'>")
+
+document.writeln("<a href='movie-single.html'><img src='http://120.79.35.160/images/mf3.jpg' alt=''/></a>")
+
+document.writeln("</div>")
+
+document.writeln("<div class='clearfix'></div>")
+
+document.writeln("</div>")
+
+document.writeln("</div><!-- end row -->")
+
+document.writeln("<hr>")
+
+document.writeln("</li> </ul> </li>")
+
+
+document.writeln("<li><a href='about.html'>关于</a></li>")
+
+document.writeln("<li><a href='contact.html'>联系我们</a></li>")
+
+document.writeln("<li><a href='faq.html'>常见问题解答</a></li>")
+
+document.writeln("<li><a href='about.html'>加入我们</a></li>")
+
+document.writeln("<!-- end dropdown w3_megamenu-fw -->")
+
+document.writeln("</ul><!-- end nav navbar-nav -->")
+
+
+document.writeln("<ul class='nav navbar-nav navbar-right'>")
+
+document.writeln("<li class='dropdown'><a href='#' data-toggle='dropdown' class='dropdown-toggle'>联系我们<b class='caret'></b></a>")
+
+document.writeln("<ul class='dropdown-menu'>")
+
+document.writeln("<li>")
+
+document.writeln("<form id='contact1' action='#' name='contactform' method='post'>")
+
+document.writeln("<div class='col-lg-6 col-md-6 col-sm-12 col-xs-12'> <input type='text' name='name' id='name1' class='form-control'placeholder='Name'><input type='text' name='email' id='email1' class='form-control'placeholder='Email'>")
+
+document.writeln("</div>")
+
+document.writeln("<div class='col-lg-6 col-md-6 col-sm-12 col-xs-12'><input type='text' name='phone' id='phone1' class='form-control'placeholder='Phone'><input type='text' name='subject' id='subject1' class='form-control'placeholder='Subject'>")
+
+document.writeln("</div>")
+
+document.writeln("<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'><textarea class='form-control' name='comments' id='comments1' rows='6'placeholder='Your Message ...'></textarea>")
+
+document.writeln("</div>")
+
+document.writeln("<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>")
+
+document.writeln("<div class='pull-right'><input type='submit' value='发送' id='submit1'class='btn btn-primary small'>")
+
+document.writeln("</div> </div>")
+
+document.writeln("<div class='clearfix'></div>")
+
+document.writeln("</form> </li> </ul> </li>")
+
+document.writeln("</ul><!-- end nav navbar-nav navbar-right -->")
+
+document.writeln("</div><!-- end #navbar-collapse-1 -->")
+
+
+document.writeln("</nav><!-- end navbar navbar-default w3_megamenu -->")
+
+document.writeln("</div><!-- end container -->")
